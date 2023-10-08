074de6aeb7f1ea61d2ae596db48458bb2cf018b1
Giovanni Martins <giovanni.martins@stone.com.br>
2023-10-08 15:42:36 -0300

    🎨 Update allowed header in CORS

--- securities/middlewares.go
+++ securities/middlewares.go
@@ -21,7 +21,19 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://"+os.Getenv("MFE_DOMAIN")+":"+os.Getenv("MFE_PORT"))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Content-Type, "+
+				"Content-Length, "+
+				"Accept-Encoding, "+
+				"X-CSRF-Token, "+
+				"Authorization, "+
+				"accept, "+
+				"origin, "+
+				"Cache-Control, "+
+				"X-Requested-With",
+		)
+
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 		if c.Request.Method == "OPTIONS" {
